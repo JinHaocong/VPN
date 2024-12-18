49e0fd99ea319fe4abd319cedb6b26da6c075fab
jhc <jinhaocong@outlook.com>
2024-12-18 18:31:50 +0800

    feat: ✨DNS缓存清除脚本更新

--- Quantumult X/Scripts/DNS-ClearCache.js
+++ Quantumult X/Scripts/DNS-ClearCache.js
@@ -1,13 +1,3 @@
-/*
- * @name = DNS-ClearCache
- * @desc = DNS-ClearCache
- * @author = crossutility
-----------------------------------------
-[task_local]
-0 0 * * * https://raw.githubusercontent.com/Centralmatrix3/Matrix-io/master/QuantumultX/Scripts/DNS-ClearCache.js, tag=清除DNS缓存, img-url=trash.circle.system, enabled=true
-----------------------------------------
-*/
-
 const message = {
     action: "dns_clear_cache"
 };
