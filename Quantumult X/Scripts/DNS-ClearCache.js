bfea06388350a8f039c97fe12729b49de11a01a8
jhc <jinhaocong@outlook.com>
2024-12-18 18:28:19 +0800

    feat: ✨DNS缓存清除脚本更新

--- Quantumult X/Scripts/DNS-ClearCache.js
+++ Quantumult X/Scripts/DNS-ClearCache.js
@@ -14,6 +14,7 @@ const message = {
 
 $configuration.sendMessage(message).then(resolve => {
     if (resolve.ret) {
+        console.log("dnsCache Cleared!");
         $done({"title": "🗑️ DNS缓存清除", "htmlMessage": 'DSN缓存已清除！'});
     } else {
         $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '清除失败！'});
