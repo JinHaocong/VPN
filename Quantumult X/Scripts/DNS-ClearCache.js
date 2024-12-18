9690e3eff5fc021579d8c4e6fb07112deadaedb9
jhc <jinhaocong@outlook.com>
2024-12-18 18:48:50 +0800

    feat: ✨增加节点信息查询脚本

--- Quantumult X/Scripts/DNS-ClearCache.js
+++ Quantumult X/Scripts/DNS-ClearCache.js
@@ -5,10 +5,10 @@ const message = {
 $configuration.sendMessage(message).then(resolve => {
     if (resolve.ret) {
         console.log("dnsCache Cleared!");
-        $done({"title": "🗑️ DNS缓存清除", "htmlMessage": 'DSN缓存已清除！'});
+        $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '<b style="color: green; font-weight: bold;">DNS缓存已清除！</b>'});
     } else {
-        $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '清除失败！'});
+        $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '<b style="color: red; font-weight: bold;">清除失败！</b>'});
     }
 }, reject => {
-    $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '清除失败！'});
+    $done({"title": "🗑️ DNS缓存清除", "htmlMessage": '<b style="color: red; font-weight: bold;">清除失败！</b>'});
 });
