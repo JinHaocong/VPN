006dc5465026d511beb17809ab70ef06170c3908
jhc <jinhaocong@outlook.com>
2025-01-15 16:17:04 +0800

    feat: ✨资源更新

--- Surge/Scripts/Connectivity-Test/index.js
+++ Surge/Scripts/Connectivity-Test/index.js
@@ -31,7 +31,7 @@ let $ = {
         // 格式化每一行
         const formattedResults = results.map(result => {
             const [name, time, emoji] = result.split(/\s+/);
-            return `${name.padEnd(15)}\t${time.padStart(5)}ms\t${emoji}`;
+            return `${name.padEnd(20)}\t${time.padStart(5)}ms\t${emoji}`;
         });
 
         // 返回所有行，用换行符连接
