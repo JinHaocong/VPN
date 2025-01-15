675f561400276140b274a30f0220c7b3c71db177
jhc <jinhaocong@outlook.com>
2025-01-15 14:22:14 +0800

    feat: ✨资源更新

--- Surge/Scripts/Sub-Info/index.js
+++ Surge/Scripts/Sub-Info/index.js
@@ -244,7 +244,7 @@ function getRmainingDays(resetDay) {
 function bytesToSize(bytes) {
     if (bytes === 0) return "0B";
     let k = 1024;
-    sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
     let i = Math.floor(Math.log(bytes) / Math.log(k));
     return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
@@ -254,5 +254,5 @@ function formatTime(time) {
     let year = dateObj.getFullYear();
     let month = dateObj.getMonth() + 1;
     let day = dateObj.getDate();
-    return numberToEmoji(year) + "年" + numberToEmoji(month) + "月" + numberToEmoji(day) + "日";
+    return year + "年" + month + "月" + day + "日";
 }
